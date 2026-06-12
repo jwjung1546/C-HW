c5d515c13c8911d1dacb3b013ae7b49c000bb785
jwjung1546 <jwjung1546@gmail.com>
2026-06-13 00:23:26 +0900

    Update TradingAnalyzer.cpp

--- ProjectJW/TradingAnalyzer.cpp
+++ ProjectJW/TradingAnalyzer.cpp
@@ -1,88 +1,109 @@
-﻿#include "TradingAnalyzer.h"
+#include "TradingAnalyzer.h"
 #include "UpbitPublicClient.h"
 #include <iostream>
 #include <cmath>
 #include <algorithm>
+#include <windows.h> 
 
 using namespace std;
 
+
 double TradingAnalyzer::minMaxScale(double X, double X_min, double X_max) {
-    if (abs(X_max - X_min) < 1e-6) {
-        return 0.5;
+    if (abs(X_max - X_min) < 1e-4 || X_max <= X_min) {
+        return 0.5; 
     }
-    return (X - X_min) / (X_max - X_min);
+    double result = (X - X_min) / (X_max - X_min);
+    if (result < 0.0 || isnan(result)) return 0.0;
+    if (result > 1.0) return 1.0;
+    return result;
 }
 
-void TradingAnalyzer::processAllMarkets(UpbitPublicClient& client) {
-    vector<CryptoAsset> rankingList; // 구조체 배열 대신 클래스 객체 벡터로 승격
-    vector<double> rawTrendScores;
-    vector<double> rawVolScores;
+void TradingAnalyzer::processAllMarkets(UpbitPublicClient& client, CryptoAsset* outResults) {
+    double rawTrendScores[8] = { 0 };
+    double rawVolScores[8] = { 0 };
+    CandleData allHistories[8][15];
 
-    const vector<string>& markets = client.getTickerList();
+    string* markets = client.getCoinList();
 
-    for (const string& market : markets) {
-        vector<CandleData> history = client.fetchDayCandles(market, 15);
+    for (int i = 0; i < 8; ++i) {
+        Sleep(100); 
 
-        if (history.size() < 10) {
-            rawTrendScores.push_back(0.0);
-            rawVolScores.push_back(0.0);
-            continue;
-        }
+        client.fetchDayCandles(markets[i], 15, allHistories[i]);
 
         double sum5 = 0.0, sum10 = 0.0;
-        for (int i = 0; i < 5; ++i)  sum5 += history[i].tradePrice;
-        for (int i = 0; i < 10; ++i) sum10 += history[i].tradePrice;
+        for (int j = 0; j < 5; ++j)  sum5 += allHistories[i][j].getTradePrice();
+        for (int j = 0; j < 10; ++j) sum10 += allHistories[i][j].getTradePrice();
 
         double ma5 = sum5 / 5.0;
         double ma10 = sum10 / 10.0;
-        double trendScore = (ma5 > ma10) ? (ma5 - ma10) / ma10 : 0.0;
-        rawTrendScores.push_back(trendScore);
+        
+  
+        double trendScore = 0.0;
+        if (ma10 > 1e-6 && ma5 > ma10) {
+            trendScore = (ma5 - ma10) / ma10;
+        }
+        rawTrendScores[i] = trendScore;
 
-        double prevHigh = history[1].highPrice;
-        double prevLow = history[1].lowPrice;
-        double todayOpen = history[0].openingPrice;
-        double todayClose = history[0].tradePrice;
-        double todayHigh = history[0].highPrice;
+
+        double prevHigh   = allHistories[i][1].getHighPrice();
+        double prevLow    = allHistories[i][1].getLowPrice();
+        double todayOpen  = allHistories[i][0].getOpeningPrice();
+        double todayClose = allHistories[i][0].getTradePrice();
+        double todayHigh  = allHistories[i][0].getHighPrice();
 
         double range = prevHigh - prevLow;
         double targetPrice = todayOpen + (range * k);
 
+
         double volatilityScore = 0.0;
-        if (todayHigh >= targetPrice) {
+        if (targetPrice > 1e-6 && todayClose > 1e-6 && todayHigh >= targetPrice) {
             volatilityScore = log(todayClose / targetPrice);
+            if (volatilityScore < 0.0 || isnan(volatilityScore)) {
+                volatilityScore = 0.0;
+            }
         }
-        rawVolScores.push_back(volatilityScore);
+        rawVolScores[i] = volatilityScore;
     }
 
-    double maxTrend = *max_element(rawTrendScores.begin(), rawTrendScores.end());
-    double minTrend = *min_element(rawTrendScores.begin(), rawTrendScores.end());
-    double maxVol = *max_element(rawVolScores.begin(), rawVolScores.end());
-    double minVol = *min_element(rawVolScores.begin(), rawVolScores.end());
+    double maxTrend = rawTrendScores[0], minTrend = rawTrendScores[0];
+    double maxVol = rawVolScores[0], minVol = rawVolScores[0];
+    for (int i = 1; i < 8; ++i) {
+        if (rawTrendScores[i] > maxTrend) maxTrend = rawTrendScores[i];
+        if (rawTrendScores[i] < minTrend) minTrend = rawTrendScores[i];
+        if (rawVolScores[i] > maxVol) maxVol = rawVolScores[i];
+        if (rawVolScores[i] < minVol) minVol = rawVolScores[i];
+    }
 
-    for (size_t i = 0; i < markets.size(); ++i) {
+  
+    for (int i = 0; i < 8; ++i) {
         double normTrend = minMaxScale(rawTrendScores[i], minTrend, maxTrend);
         double normVol = minMaxScale(rawVolScores[i], minVol, maxVol);
-
         double finalScore = (normTrend * trendWeight) + (normVol * volatilityWeight);
+        
+        if (isnan(finalScore)) finalScore = 0.0; 
 
-        // 클래스 생성자를 주입하여 인스턴스 자동 등록
-        rankingList.push_back(CryptoAsset(markets[i], finalScore));
+        outResults[i] = CryptoAsset(markets[i], finalScore, allHistories[i]);
     }
 
-    // 람다 식을 통한 클래스 객체 내림차순 정렬 연산
-    sort(rankingList.begin(), rankingList.end(), [](const CryptoAsset& a, const CryptoAsset& b) {
-        return a.getScore() > b.getScore();
-        });
+  
+    for (int i = 0; i < 7; ++i) {
+        for (int j = 0; j < 7 - i; ++j) {
+            if (outResults[j].getScore() < outResults[j + 1].getScore()) {
+                CryptoAsset temp = outResults[j];
+                outResults[j] = outResults[j + 1];
+                outResults[j + 1] = temp;
+            }
+        }
+    }
 
     cout << "\n=========================================" << endl;
-    cout << "★ [실시간 업비트 오픈 API 연동] 가상화폐 추천 순위 ★" << endl;
+    cout << " [실시간 업비트 오픈 API 연동] 가상화폐 추천 순위 " << endl;
     cout << "=========================================" << endl;
-    for (size_t i = 0; i < rankingList.size(); ++i) {
-        // 클래스의 getKoreanName() 기능을 써서 우아하게 한글로 전광판 렌더링
-        cout << i + 1 << "위: " << rankingList[i].getKoreanName()
-            << " (" << rankingList[i].getScore() * 100.0 << "점)" << endl;
+    for (int i = 0; i < 8; ++i) {
+        cout << i + 1 << "위: " << outResults[i].getKoreanName()
+             << " (" << outResults[i].getScore() * 100.0 << "점)" << endl;
     }
     cout << "-----------------------------------------" << endl;
-    cout << "📢 실시간 서버 분석 기준 최상위 추천 종목: [" << rankingList[0].getKoreanName() << "]" << endl;
+    cout << " [안내] 현재 최고 점수 종목: [" << outResults[0].getKoreanName() << "]" << endl;
     cout << "=========================================" << endl;
 }
