d9153cc118acd70711ab77405272197e0b917660
jwjung1546 <jwjung1546@gmail.com>
2026-06-13 00:21:35 +0900

    Update UpbitPublicClient.h

--- ProjectJW/UpbitPublicClient.h
+++ ProjectJW/UpbitPublicClient.h
@@ -1,7 +1,6 @@
 #ifndef UPBIT_PUBLIC_CLIENT_H
 #define UPBIT_PUBLIC_CLIENT_H
 
-#include <vector>
 #include <string>
 #include "CandleData.h"
 
@@ -9,12 +8,14 @@ using namespace std;
 
 class UpbitPublicClient {
 private:
-    vector<string> tickerList;
+    string CoinList[8]; 
 
 public:
     UpbitPublicClient();
-    const vector<string>& getTickerList() const;
-    vector<CandleData> fetchDayCandles(string marketCode, int count);
+
+    string* getCoinList();
+
+    void fetchDayCandles(string marketCode, int count, CandleData* outHistory);
 };
 
 #endif
