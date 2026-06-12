a5b70115d1ce507899ad139631ad0c1a3668d17d
jwjung1546 <jwjung1546@gmail.com>
2026-06-13 00:24:20 +0900

    Update main.cpp

--- ProjectJW/main.cpp
+++ ProjectJW/main.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include "UpbitPublicClient.h"
 #include "TradingAnalyzer.h"
 
@@ -8,7 +8,32 @@ int main() {
     UpbitPublicClient client;
     TradingAnalyzer analyzer;
 
-    analyzer.processAllMarkets(client);
+    CryptoAsset results[8];
+
+    analyzer.processAllMarkets(client, results);
+
+    while (true) {
+        cout << "\n[메뉴] 추세 상태를 볼 코인 번호(1~8) 입력 (종료: 0) -> ";
+        int choice;
+        cin >> choice;
+
+        if (choice == 0) {
+            cout << "프로그램을 종료합니다. 성공적인 투자 되세요!" << endl;
+            break;
+        }
+
+
+
+        CryptoAsset& selected = results[choice - 1];
+
+        cout << "\n=================================================================" << endl;
+        cout << " [추세 분석] " << selected.getKoreanName() << " (" << selected.getCode() << ")" << endl;
+        cout << "-----------------------------------------------------------------" << endl;
+        
+        selected.printQuantReport(); // 5시간/10시간선 이평선 보고서 즉각 출력
+        
+        cout << "=================================================================" << endl;
+    }
 
     return 0;
 }
