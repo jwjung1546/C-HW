c13c42f3ed130f22ff1d2ac2a30e3c612cd8a62e
jwjung1546 <jwjung1546@gmail.com>
2026-06-13 00:22:34 +0900

    Update TradingAnalyzer.h

--- ProjectJW/TradingAnalyzer.h
+++ ProjectJW/TradingAnalyzer.h
@@ -1,23 +1,21 @@
 #ifndef TRADING_ANALYZER_H
 #define TRADING_ANALYZER_H
 
-#include <vector>
 #include "CandleData.h"
 
-using namespace std;
-
-class UpbitPublicClient;
+class UpbitPublicClient; 
 
 class TradingAnalyzer {
 private:
-    static constexpr double trendWeight = 0.7;
-    static constexpr double volatilityWeight = 0.3;
-    static constexpr double k = 0.5;
+    static constexpr double trendWeight = 0.7;       
+    static constexpr double volatilityWeight = 0.3;  
+    static constexpr double k = 0.5;               
 
     static double minMaxScale(double X, double X_min, double X_max);
 
 public:
-    void processAllMarkets(UpbitPublicClient& client);
+  
+    void processAllMarkets(UpbitPublicClient& client, CryptoAsset* outResults);
 };
 
 #endif
